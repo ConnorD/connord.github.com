47dd1d5c600f4047ff12640d056e516a4ced76d0
Connor Denman <connor.denman@gmail.com>
2010-11-27 17:34:45 -0700

    added Rails site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConnorGithub::Application.config.secret_token = '88ce4f244ee66669125014a6c180ce2307d9b3b129e955785f4a70ac912c1661bf9700d6b40e1ec382f0102066fc126521fa6cd9c430946c3176d534c30e84ca'
